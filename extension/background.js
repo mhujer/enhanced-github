3d24df6e443b70dec5a4ae55247ab503d5d5ae8f
Martin Hujer <mhujer@gmail.com>
2014-12-12 19:00:31 +0100

    comment

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,4 @@
+//github uses pushState and not a proper navigation, so this is necessary (http://stackoverflow.com/a/21071357/282834)
 chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
     chrome.tabs.executeScript(null, {file: 'enhanced-github.js'});
 });
